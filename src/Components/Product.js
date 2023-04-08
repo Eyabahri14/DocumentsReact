49de0e92945ec4243a6c3bcd47a222d73b49f5e1
Eyabahri14 <eya.bahri@esprit.tn>
2023-04-08 03:31:15 +0100

    transforming the product into a functionnal component

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,62 +1,53 @@
-import {React,Component} from 'react'
-import {Card,Col,Row,Button} from 'react-bootstrap'
-
-
-class Product extends Component {
-
-   
-  state = {
+import { React, useState } from 'react';
+import { Card, Col, Row, Button } from 'react-bootstrap';
+//from class component to functionnal component 
+function Product(props) {
+  const [product, setProduct] = useState(props.product);
  
-    like: this.props.product.like,
-    quantity: this.props.product.quantity,
-    ...this.props.product,
-  }
+  const addLike = () => {
+    setProduct({ 
+      //don't forget the spread
+      ...product,
+      
+      like: product.like + 1 });
+  };
 
-  addLike = () => {
-    this.setState({
-      ...this.state, //pour charger aussi les autres attributs de product quand like change
-      like: this.state.like + 1
-    })
-  }
+  
 
-
-
-  render() {
-    return (
-      <div>
-         <Card style={{ width: "18rem" }} className="text-center" border="secondary">
-
+  return (
+    <div>
+     
+      <Card  style={{ width: '18rem' }}   className={"text-center" + (product.like >= 6 ? " bestProduct" : "")} border="secondary">
         <Card.Header>
-        <Card.Img
-          variant="top"
-          src={require("../assets/images/" + this.state.img)}
-          alt="Product Img"
-          height={200}
-        />               
+          <Card.Img variant="top" src={require(`../assets/images/${product.img}`)} alt="Product Img" height={200} />
         </Card.Header>
 
         <Card.Body>
-                    <Card.Title>Title : {this.state.name}</Card.Title>
-                    <Card.Text>Likes : {this.state.like }</Card.Text>
-                    <Card.Text>Quantity : {this.state.quantity }</Card.Text>
-                    <Card.Text>Description : {this.state.description }</Card.Text>
-                    <Card.Text>Price : {this.state.price} DT</Card.Text>
+          <Card.Title>Title : {product.name}</Card.Title>
+          <Card.Text>Likes : {product.like}</Card.Text>
+          <Card.Text>Quantity : {product.quantity}</Card.Text>
+          <Card.Text>Description : {product.description}</Card.Text>
+          <Card.Text>Price : {product.price} DT</Card.Text>
 
-                    <Row className="justify-content-center align-items-center m-4">
-
-                    <div className="btn-group" >
-                    <Button onClick={this.addLike} className="btn btn-danger m-2">Like</Button>
-                    <Button onClick={() => this.props.buyFunction(this.state)} disabled={this.state.quantity === 0} className="btn btn-warning m-2">Buy</Button>
-
-                    </div>
-
-                   </Row>
-                    
-          </Card.Body>
-          </Card>
-      </div>
-    )
-  }
+          <Row className="justify-content-center align-items-center m-4">
+            <div className="btn-group">
+              <Button onClick={addLike} className="btn btn-danger m-2">
+                Like
+              </Button>
+              <Button
+                onClick={() => props.buyFunction(product)}
+                disabled={product.quantity === 0}
+                className="btn btn-warning m-2"
+              >
+                Buy
+              </Button>
+            </div>
+          </Row>
+        </Card.Body>
+      </Card>
+  
+    </div>
+  );
 }
 
 export default Product;
